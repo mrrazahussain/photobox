2e54950cba308dee778d8f098290f7f5373d491c
Raza Hussain <mr.raza.hussain@gmail.com>
2012-12-15 16:31:48 +0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photobox::Application.config.secret_token = 'b08757ce7216e219cf230b18e9384c6b75147ac5dc389b68f0230db4f341b4f28631ba243828535559e4d6b833488577c70b3c991c2e272c31e79a253f800a5d'
