c24b13eb9970df4a3857414d0ec5e86e2162f8ca
Raza Hussain <mr.raza.hussain@gmail.com>
2012-12-16 16:52:38 +0500

    seed file modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,9 @@ Role.create([
                 {:name => 'moderator'}
             ], :without_protection => true)
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
+user = User.create! :name => 'Administrator', :email => 'raza.mking@nxvt.com', :password => 'password', :password_confirmation => 'password'
 puts 'New user created: ' << user.name
-user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
+user2 = User.create! :name => 'test', :email => 'test2@example.com', :password => 'password', :password_confirmation => 'password'
 puts 'New user created: ' << user2.name
 user.add_role :admin
 user2.add_role :moderator
